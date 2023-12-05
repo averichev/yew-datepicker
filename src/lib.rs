68d2b09d6bbc3cf4d1ec59661ed1fb9373acc0ab
averichev <averichev@yandex.ru>
2023-12-05 17:27:48 +0300

    style: Format

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use chrono::{Locale, Weekday};
 use chrono::{Datelike, NaiveDate};
+use chrono::{Locale, Weekday};
 use chronoutil::shift_months;
 use std::convert::TryFrom;
 use yew::{html, Callback, Component, Context, Html, Properties};
